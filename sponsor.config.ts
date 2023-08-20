567298ca25c97c8ea08937980c0620ae07d161af
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-19 20:35:53 -0700

    add sponsor svg chron job

---
+++ sponsor.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    {
+      title: 'Backers',
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: presets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
+})
